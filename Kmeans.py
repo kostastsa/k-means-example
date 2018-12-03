ca481be26cd382e428c32c656893d68476cf076c
kostastsa <33462414+kostastsa@users.noreply.github.com>
2018-12-02 19:57:07 -0800

    Add files via upload

--- Kmeans.py
+++ Kmeans.py
@@ -2,34 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#mean_table =  np.transpose([np.random.uniform(0,1,nclust),np.random.uniform(0,1,nclust)])
-#cov_table = [[[0.005,a],[a,0.005]] for a in [0*np.random.uniform(0,1) for i in range(nclust)]]
-#data = [np.random.multivariate_normal(mean_table[i], cov_table[i], npclust) for i in range(nclust)]
-
+m=2000
 class datapoint:
-    __ind=0
-    def __init__(self,mu,cov):
-        self.index = datapoint.__ind
+
+    def __init__(self,mu,cov,ind):
+        self.index = ind
         self.pos = np.random.multivariate_normal(mu,cov,1)
-        datapoint.__ind+=1
+
 
     def __str__(self):
-        print(self.index,self.pos)
-
-
-
-
-
-
-# data_join=[]
-# for i in range(nclust):
-#     for j in range(len(data[i])):
-#         data_join.append(list(data[i][j]))
-
-# data_join = np.array(data_join).transpose()
-
-## K - means starts here
-# Initialization of cluster centroids
+        return 'index={},pos={}'.format(self.index,self.pos)
 
 def init_centroids(m,nclust):
     choice=np.random.choice(m,nclust,replace=False)
@@ -58,6 +40,7 @@ def segregate_clusters(clust_assignments,data_points,m):
                 clust_assignments {[type]} -- [description]
                 data_points {[type]} -- [description]
         """
+        k=2
         points_in_cluster=[]
         for j in range(k):
                 points_in_cluster.append([data_points[i].index for i in range(m) if clust_assignments[i]==j])
@@ -67,69 +50,85 @@ def segregate_clusters(clust_assignments,data_points,m):
 def move_centroid(points_in_cluster,k):
         new_centroids = []
         for c in range(k):
-            clust = points_in_cluster[c]
-            s = len(clust)
-            new_centroids.append(sum(clust)/s)
+            clust0 = points_in_cluster[c]
+            s = len(clust0)
+            new_centroids.append(sum(clust0)/s)
         return new_centroids
 
 
-def misclasification(init_data, sep_lists):
-    return
+def similarities(real_ind, clust_ind):
+    clust1=clust_ind[1]
+    simils=[]
+    for data_clust in real_ind:
+        count_sim=0
+        for i in data_clust:
+            if i in clust1: count_sim+=1
+        simils.append(count_sim)
+    return simils
 
 
-nclust = 2
-npclust = 10000
-d=0.51
-
-mu1 = np.array([-d/2,0])
-mu2 = np.array([+d/2,0])
-cov=[[0.05,0],[0,0.05]]
-data1 = [datapoint(mu1,cov) for i in range(npclust)]
-data2 = [datapoint(mu2,cov) for i in range(npclust)]
-data=data1+data2
-data1_pos = [p.pos for p in data1]
-data2_pos = [p.pos for p in data2]
-data_pos = data1_pos+data2_pos
-m = len(data_pos)
-
-
-k=nclust
-i_label = [int(init_centroids(m,nclust)[0]),int(init_centroids(m,nclust)[1])]
-centroids = np.array([data_pos[i_label[0]],data_pos[i_label[1]]])
-
-dt=1
-while dt>0.001:
-    old_centroids = centroids
-    assignments = assign_cluster(data_pos, centroids)
-    sep_lists_ind = segregate_clusters(assignments, data, m)
-    sep_lists=[]
-    for clust in sep_lists_ind:
-        sep_lists.append([data[i].pos for i in clust])
-    plt.show()
-    plt.close()
-    centroids = move_centroid(sep_lists,k)
-    dt=np.linalg.norm(np.array(centroids)-np.array(old_centroids))
+def misclassification_error(d,sigma):
+    nclust = 2
+    npclust = 1000
+    m=nclust*npclust
+    mu1 = np.array([-d/2,0])
+    mu2 = np.array([+d/2,0])
+    cov=[[sigma,0],[0,sigma]]
+    data1 = [datapoint(mu1,cov,i) for i in range(npclust)]
+    data2 = [datapoint(mu2,cov,npclust+i) for i in range(npclust)]
+    data = data1 + data2
+    data1_pos = [p.pos for p in data1]
+    data2_pos = [p.pos for p in data2]
+    data_pos = data1_pos+data2_pos
+    k = nclust
+    i_label = [int(init_centroids(m,nclust)[0]),int(init_centroids(m,nclust)[1])]
+    centroids = np.array([data_pos[i_label[0]],data_pos[i_label[1]]])
+    dt=1
+    while dt>0.001:
+        old_centroids = centroids
+        assignments = assign_cluster(data_pos, centroids)
+        sep_lists_ind = segregate_clusters(assignments, data, m)
+        sep_lists=[]
+        for clust in sep_lists_ind:
+            sep_lists.append([data[i].pos for i in clust])
+        centroids = move_centroid(sep_lists,k)
+        dt=np.linalg.norm(np.array(centroids)-np.array(old_centroids))
 
 
-c1_ind = [p.index for p in data1]
-c2_ind = [p.index for p in data2]
-count=0
-for p in sep_lists_ind[1]:
-    if p in c1_ind:
-        count+=1
-print('count=',count)
-print(len(sep_lists_ind[1]))
+    c1_ind = [p.index for p in data1]
+    c2_ind = [p.index for p in data2]
+    data_ind=[c1_ind,c2_ind]
+    sim = similarities(data_ind, sep_lists_ind)
+    error = 1 - max(sim)/npclust
+    return error
 
-f, (ax1, ax2) = plt.subplots(1, 2)
-data=np.array([data1_pos,data2_pos])
-for i in range(k):
-    datap1 = np.transpose(data[i])
-    ax1.scatter(datap1[0],datap1[1],s=1)
-    datum = np.array(sep_lists[i]).transpose()
-    ax2.scatter(datum[0],datum[1],s=1)
+
+
+errors=[]
+dist=[]
+t=0
+e=1
+sigma=0.05
+while e>0.0001:
+   e = misclassification_error(t*0.01/0.05,sigma)
+   errors.append(e)
+   dist.append(t*0.01/0.05)
+   t+=1
+plt.plot(dist,errors)
 plt.show()
 
+#print(misclassification_error(0.1))
 
+
+
+# f, (ax1, ax2) = plt.subplots(1, 2)
+# data=np.array([data1_pos,data2_pos])
+# for i in range(k):
+#     datap1 = np.transpose(data[i])
+#     ax1.scatter(datap1[0],datap1[1],s=1)
+#     datum = np.array(sep_lists[i]).transpose()
+#     ax2.scatter(datum[0],datum[1],s=1)
+# plt.show()
 
 
 
